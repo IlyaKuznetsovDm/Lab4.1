5f1babd11a5926c348f1679a82ed5ce36fe5b9d8
Dmitr <Dmitr@DESKTOP-H2P4N61>
2021-11-30 14:23:16 +0200

    Добавьте файлы проекта.

---
+++ Lab4.1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace oneAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            //4.1- 8 вариант - базовый уровень
+            double[] m = new double[16];
+            Random rnd = new Random();
+            Console.WriteLine("Вывод - 16 элементов массива: ");
+            Console.WriteLine("------------------------------");
+            for (int i = 0; i < m.Length; i++)
+            {
+                m[i] = rnd.Next(16);
+                Console.WriteLine(m[i] + " ");
+
+            }
+            double min = m[0];
+            double max = m[0];
+            for (int i = 0; i < m.Length; i++)
+            {
+                if (max < m[i])
+                {
+                    max = m[i];
+                }
+                if (min > m[i])
+                {
+                    min = m[i]; 
+                }
+            }          
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("Результат:");           
+            Console.WriteLine("Мин.элемент = {0} ", min);
+            Console.WriteLine("Макс.элемент = {0} ", max);
+            Console.WriteLine("------------------------------");
+        }
+    }
+}
